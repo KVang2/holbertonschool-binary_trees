acdfe1359c4a9c4b28594b062286d520f88b9809
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-22 14:23:46 -0800

    binary insert

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,7 +23,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 	{
-		return(NULL);
+		free(new);
+		return (NULL);
 	}
 
 	if (parent->left != NULL)
