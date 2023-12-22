8e619214497ca9dedf95ba6a249570d7f454ca83
Kevin Vang <kevinvaj88@hotmail.com>
2023-12-21 23:20:38 -0800

    0-tree

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent tree
+ * @value: int value
+ * Return: pointer to new node or NULL on failure
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+{
+	binary_tree_t *new;
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL);
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = NULL;
+	return (new);
+}
